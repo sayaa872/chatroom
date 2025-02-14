905b5344eeff77bb0d98144b2aca5b0057a722b9
sayaa872 <dbt.sayajen@gmail.com>
2025-02-14 16:28:28 +0100

    fhueizfbi

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('serveur en marche sur http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`serveur en marche sur le port ${PORT}`);
 });
